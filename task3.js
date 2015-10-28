cad6176d27409c00e26afacafe9434acf32fca78
Andrii <andrewmelnyk9292@gmail.com>
2015-10-28 14:10:06 +0300

    pifagor and yura task

---
+++ task3.js
@@ -0,0 +1,14 @@
+
+function pifagor (height, width) {
+	 var pifagor = [];
+ for (var i = 1 ; i<=height;i++) {
+ 	pifagor[i]=[];
+  for (var z = 1 ; z<= width;z++) {
+  	pifagor[i].push(i*z);
+   
+  }
+ }
+ console.log(pifagor);
+}
+pifagor (2, 2); 
+
