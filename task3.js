f9defd9325532da1741c2f12ed8c717d311ef41d
Andrii <andrewmelnyk9292@gmail.com>
2015-10-28 15:04:46 +0300

    pifagor and yura task tab change

--- task3.js
+++ task3.js
@@ -1,12 +1,11 @@
 function pifagor (height, width) {
-	var pifagor = [];
-		for (var i = 1 ; i<=height;i++) {
-			pifagor[i]=[];
-		for (var z = 1 ; z<= width;z++) {
-			pifagor[i].push(i*z);
-		}
-		}
-			console.log(pifagor);
+    var pifagor = [];
+        for (var i = 1 ; i <= height;i++) {
+            pifagor[i]=[];
+            for (var z = 1 ; z <= width;z++) {
+                pifagor[i].push(i*z);
+            }
+        }
+        console.log(pifagor);
 }
-pifagor (2, 2); 
-
+pifagor(2, 2);
